0565972ae180f36bc53ee1a2c8943f50b794f335
James Farris <jafarris.exe@gmail.com>
2023-04-29 04:10:40 -0400

    Added prompts for generator using inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,72 @@
 // TODO: Include packages needed for this application
-
+import { default as inquirer } from "inquirer";
+import { writeFile } from "fs";
+import { generateMarkdown } from "./utils/generateMarkdown";
 // TODO: Create an array of questions for user input
-const questions = [];
+// Create license options in an array and apply their value to dropdown selection
+const options = [
+  { name: "MIT", value: "mit" },
+  { name: "GPLv2", value: "gplv2" },
+  { name: "GPLv3", value: "gplv3" },
+  { name: "Apache", value: "apache" },
+  { name: "BSD 3-Clause", value: "BSD 3-Clause" },
+];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your Project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give me a short description of what it can do.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How do I install it?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do I use it? Explain like I'm 5.",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What are your guidelines for contributions?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message:
+      "How can somebody test it? Make sure to include all commands and such that are required.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Would you like to use a license?",
+    choices: options,
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+writeFile("README.md", generateMarkdown, (err) => {
+  err
+    ? console.error(err)
+    : console.log("Generated your new README! Good luck!");
+});
 
 // TODO: Create a function to initialize app
 function init() {}
